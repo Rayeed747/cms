5f42cd4c50e8792da244fac6e1efa6c0614ae47f
Rayeed747 <48745632+Rayeed747@users.noreply.github.com>
2021-09-23 12:26:23 +0600

    upload

---
+++ app/Models/Model/user/category_post.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Model\user;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class category_post extends Model
+{
+    use HasFactory;
+}
